43e3dde5731a2b8fc0245cac03922ddc325c1536
IgnaDev <arcortoon@gmail.com>
2023-10-19 11:46:11 -0300

    importe de react y componetnes

---
+++ src/components/ButtonSubscribe.jsx
@@ -0,0 +1,22 @@
+import {useState} from 'react'
+import '../style/botton.css'
+import { toast } from 'react-toastify';
+
+
+export default function ButtonSubscribe() {
+    const [subs, setSubs] = useState(false)
+
+  return (
+    <>
+        <button 
+        className='button'
+        onClick={()=>{
+            toast.info(`Tu estas ${subs ? 'des' : ''}subscripto`)
+            setSubs(!subs)
+        }}
+        >{
+            subs ? 'Suscríbete' : 'Subscripto'
+        }</button>
+    </>
+  )
+}
